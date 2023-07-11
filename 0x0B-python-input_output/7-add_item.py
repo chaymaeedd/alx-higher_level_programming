b50c07d03e8f5499c17bc67d5a1e98cc8253affe
Chaymaeedd <c.eddouks@gmail.com>
2023-07-11 16:12:30 -0700

    TT

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,15 +1,3 @@
-import json
-
-# Create a Python list
-my_list = ["item1", "item2", "item3"]
-
-# Convert the list to a JSON-formatted string
-json_string = json.dumps(my_list)
-
-# Open the file in write mode and write the JSON string to it
-with open("add_item.json", "w") as file:
-    file.write(json_string)
-
 #!/usr/bin/python3
 """
 Script that adds all arguments to a Python list, and then saves them to a file
